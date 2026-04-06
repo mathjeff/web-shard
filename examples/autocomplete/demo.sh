f2c7ef0cfbf512375df989b2979698aad68420f3
Jeffry Gaston <gastoj3@gmail.com>
2026-04-06 15:59:04 +0800

    preliminary autocomplete demo

---
+++ examples/autocomplete/demo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+cd "$(dirname $0)"
+projectRoot="./"
+repoRoot="../../"
+
+$repoRoot/generate.py --input-data $projectRoot/src/data.json --out-site $projectRoot/out/data --overwrite --entries-per-shard 4
+cp $repoRoot/web-shard.js $projectRoot/out/
+cp $repoRoot/deps/autoComplete.min.js $projectRoot/out/
+cp $projectRoot/src/index.html $projectRoot/out/
+
+
+echo
+echo "The website has been created in $projectRoot/"
+echo
+echo "Now open http://localhost:8000/out/index.html in a web browser"
+echo
+echo "Press ctrl-C when done"
+echo
+echo "Starting a webserver to show the website"
+echo
+
+python -m http.server
