03a423516a49b3b56d24d0453d49e6d5d5cf9cfe
Jeffry Gaston <gastoj3@gmail.com>
2026-04-06 11:43:52 +0800

    more demo data and larger demo shards to better demonstrate usage

--- demo.sh
+++ demo.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname $0)"
 
-./generate.py --input-data example/src/data.json --out-site example/out/data --overwrite --entries-per-shard 2
+./generate.py --input-data example/src/data.json --out-site example/out/data --overwrite --entries-per-shard 4
 cp ./loader.js example/out/
 cp ./example/src/index.html example/out/
 
